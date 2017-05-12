e1e5973ae7ebdd59bcecb9f94368a7335c4cb0c3
Hiroki Kumamoto <kumabook@live.jp>
2017-05-12 21:03:04 +0900

    Fix public path

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', index);
 
 app.use((req, res, next) => {
