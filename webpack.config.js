581a4b29a5c5e37bed0af9caea274e86065d5161
Hiroki Kumamoto <kumabook@live.jp>
2017-05-12 21:00:42 +0900

    Setup webpack and frontend lib

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path    = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry:   './src/client.jsx',
+  output:  {
+    path:       path.resolve(__dirname, 'public'),
+    filename:   'bundle.js',
+    publicPath: '/public/',
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test:    /.jsx?$/,
+        loader:  'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+      },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
