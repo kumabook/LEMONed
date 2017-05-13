9e97f4e2ee84f98df4309fdafd902f5853ff54b8
Hiroki Kumamoto <kumabook@live.jp>
2017-05-13 15:12:34 +0900

    Rename tsc tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,9 @@ const gulp        = require('gulp');
 const mocha       = require('gulp-mocha');
 const browserSync = require('browser-sync');
 const nodemon     = require('gulp-nodemon');
-const tsb         = require('gulp-tsb');
 
-gulp.task('test', ['tsc:test'],
-          () => gulp.src('./test_dist/test*.js', { read: false })
-                    .pipe(mocha()));
+gulp.task('test', () => gulp.src('./test/*.js', { read: false })
+                            .pipe(mocha()));
 
 gulp.task('browser-sync', ['nodemon', 'watch'], () => browserSync.init(null, {
   proxy: 'http://localhost:4000',
@@ -27,17 +25,6 @@ gulp.task('nodemon', (cb) => {
   }).on('restart', () =>
         setTimeout(() => browserSync.reload({ stream: false }), 500));
 });
-
-const tsbSrc = tsb.create('src/tsconfig.json');
-gulp.task('build', () => gulp.src('./src/**/*.ts')
-                             .pipe(tsbSrc())
-                             .pipe(gulp.dest('./dist')));
-
-const tsbTest = tsb.create('test/tsconfig.json');
-gulp.task('tsc:test', () => gulp.src('./test/**/*.ts')
-                                .pipe(tsbTest())
-                                .pipe(gulp.dest('./test_dist')));
-
 
 gulp.task('watch', () => {
   gulp.watch('src/**/*.ts', ['build']);
