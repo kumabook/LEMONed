bd829da804aec2c0d44e3828dd0658a793b00db2
Hiroki Kumamoto <kumabook@live.jp>
2017-05-24 17:29:02 +0900

    App container component manages progress

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,40 +1,62 @@
 import * as React           from 'react';
+import PropTypes            from 'prop-types';
 import { connect }          from 'react-redux';
 import AppBar               from 'material-ui/AppBar';
 import Snackbar             from 'material-ui/Snackbar';
 import Drawer               from 'material-ui/Drawer';
 import MenuItem             from 'material-ui/MenuItem';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd           from 'material-ui/svg-icons/content/add';
+import CircularProgress     from 'material-ui/CircularProgress';
 import { withRouter }       from 'react-router-dom';
 import {
   toggleDrawler,
   closeMessage,
-} from '../actions';
+} from '../actions/app';
+
+const progressStyle = {
+  position:        'absolute',
+  top:             0,
+  left:            0,
+  width:           '100%',
+  height:          '100%',
+  backgroundColor: 'rgba(10, 10, 10, 0.4)',
+  zIndex:          2000,
+};
 
 class App extends React.Component {
+  static get propTypes() {
+    return {
+      handleLogoutMenuClick:  PropTypes.func.isRequired,
+      handleSignUpMenuClick:  PropTypes.func.isRequired,
+      handleArtistsMenuClick: PropTypes.func.isRequired,
+      handleTracksMenuClick:  PropTypes.func.isRequired,
+      handleClick:            PropTypes.func.isRequired,
+      handleMessageClose:     PropTypes.func.isRequired,
+      drawlerIsOpen:          PropTypes.bool.isRequired,
+      progress:               PropTypes.bool.isRequired,
+      title:                  PropTypes.string.isRequired,
+      message:                PropTypes.string.isRequired,
+      isLoggedIn:             PropTypes.bool.isRequired,
+      children:               PropTypes.array.isRequired,
+    };
+  }
   render() {
-    const plusButton = (
-      <FloatingActionButton
-        secondary
-        mini
-        onTouchTap={this.props.handlePlusButtonClick}
-      >
-        <ContentAdd />
-      </FloatingActionButton>
-    );
     const menuItems = [];
     if (this.props.isLoggedIn) {
       menuItems.push(<MenuItem key="logout" onTouchTap={this.props.handleLogoutMenuClick}>Logout</MenuItem>);
     } else {
       menuItems.push(<MenuItem key="signup" onTouchTap={this.props.handleSignUpMenuClick}>Sign Up</MenuItem>);
     }
+    menuItems.push(<MenuItem key="artists" onTouchTap={this.props.handleArtistsMenuClick}>Artists</MenuItem>);
     menuItems.push(<MenuItem key="tracks" onTouchTap={this.props.handleTracksMenuClick}>Tracks</MenuItem>);
+    const progress = !this.props.progress ? null : (
+      <div style={progressStyle}>
+        <CircularProgress style={{ marginTop: '45%', marginLeft: '49%' }} />
+      </div>
+    );
     return (
       <div>
         <AppBar
           title={this.props.title}
-          iconElementRight={plusButton}
           onLeftIconButtonTouchTap={this.props.handleClick}
         />
         <Drawer open={this.props.drawlerIsOpen}>
@@ -51,6 +73,7 @@ class App extends React.Component {
           onRequestClose={this.props.handleMessageClose}
         />
         {this.props.children}
+        {progress}
       </div>
     );
   }
@@ -62,23 +85,25 @@ function mapStateToProps(state) {
     drawlerIsOpen: state.app.drawlerIsOpen,
     message:       state.app.message,
     isLoggedIn:    state.app.isLoggedIn,
+    progress:      state.app.progress,
   };
 }
 
 function mapDispatchToProps(dispatch, { history }) {
   return {
     handleClick:           () => dispatch(toggleDrawler()),
-    handlePlusButtonClick: () => {
-      history.push('/tracks/new');
-    },
     handleLogoutMenuClick: () => {
       window.location.href = '/logout';
     },
     handleSignUpMenuClick: () => {
       window.location.href = '/signup';
     },
+    handleArtistsMenuClick: () => {
+      history.push({ pathname: '/artists' });
+      dispatch(toggleDrawler());
+    },
     handleTracksMenuClick: () => {
-      history.push({ pathname: '/tracks', query: { page: 0 } });
+      history.push({ pathname: '/tracks' });
       dispatch(toggleDrawler());
     },
     handleMessageClose: () => {
--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,27 +1,43 @@
 /* global flashMessage, title, isLoggedIn */
-const initialState = {
-  drawlerIsOpen: false,
-  message:       flashMessage || '',
-  title:         title || '',
-  isLoggedIn:    isLoggedIn || false,
-};
 
-export default (state = initialState, action) => {
+import { combineReducers } from 'redux';
+
+const drawlerIsOpen = (state = false, action) => {
   if (action.type === 'TOGGLE_DRAWLER') {
-    return {
-      drawlerIsOpen: !state.drawlerIsOpen,
-      message:       state.message,
-      title:         state.title,
-      isLoggedIn:    state.isLoggedIn,
-    };
-  }
-  if (action.type === 'CLOSE_MESSAGE') {
-    return {
-      drawlerIsOpen: state.drawlerIsOpen,
-      message:       '',
-      title:         state.title,
-      isLoggedIn:    state.isLoggedIn,
-    };
+    return !state;
   }
   return state;
 };
+
+const progress = (state = false, action) => {
+  switch (action.type) {
+    case 'SHOW_PROGRESS':
+      return true;
+    case 'HIDE_PROGRESS':
+      return false;
+    default:
+      return state;
+  }
+};
+
+const message = (state = flashMessage || '', action) => {
+  switch (action.type) {
+    case 'SHOW_MESSAGE':
+      return action.payload;
+    case 'CLOSE_MESSAGE':
+      return '';
+    default:
+      return state;
+  }
+};
+
+const titleReducer = (state = title || '') => state;
+const isLoggedInReducer = (state = isLoggedIn) => state;
+
+export default combineReducers({
+  drawlerIsOpen,
+  message,
+  progress,
+  title:      titleReducer,
+  isLoggedIn: isLoggedInReducer,
+});
