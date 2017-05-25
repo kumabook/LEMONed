5927d152f193d49ff9aecac9990c48f4789ade66
Hiroki Kumamoto <kumabook@live.jp>
2017-05-26 02:16:16 +0900

    Use material-jsonschema Table component

--- src/containers/TrackList.jsx
+++ src/containers/TrackList.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { push } from 'react-router-redux';
-import Table from '../components/Table';
+import { Table } from 'material-jsonschema';
 import { schema, tableSchema, formSchema } from '../models/track';
 import { creators } from '../actions/track';
 import { defaultPerPage } from '../config';
