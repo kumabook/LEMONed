21556f738f43ecca6b8b483c35504c56949925b6
Hiroki Kumamoto <kumabook@live.jp>
2017-05-26 02:12:13 +0900

    Remove generic json schema model components beacuse it is extracted as `material-jsonschema`

--- src/utils/date.js
+++
@@ -1,8 +0,0 @@
-export default {
-  parse: (str) => {
-    if (str) {
-      return new Date(str);
-    }
-    return new Date();
-  },
-};
--- src/utils/url.js
+++
@@ -1,9 +0,0 @@
-const defaultPorts = { 'http:': 80, 'https:': 443 };
-
-export const getRootUrl = () => {
-  const { port, protocol, hostname } = window.location;
-  const p = (port && (port !== defaultPorts[protocol])) ? `:${port}` : '';
-  return `${protocol}//${hostname}${p}`;
-};
-
-export const getFileUrl = path => `${getRootUrl()}${path}`;
