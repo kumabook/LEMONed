e7473c7679e95fbcc4f4d1330fe4b87675793c73
Hiroki Kumamoto <kumabook@live.jp>
2017-05-25 15:42:05 +0900

    Define process.env.NODE_ENV with webpack.DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
   ],
   module: {
     rules: [
